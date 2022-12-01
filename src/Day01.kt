d323415b76f9c4cfa836de355e2b5cb552c4461f
Garrett <g@garrettshorr.com>
2022-11-30 21:13:26 -0800

    day 1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,47 @@
+import java.lang.Exception
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun part1(input: List<String>): Int {
+    val elves = mutableListOf<Int>()
+    var totalCalories = 0
+    for(food in input) {
+      if (food != "") {
+        totalCalories += food.toInt()
+      } else {
+        elves.add(totalCalories)
+        totalCalories = 0
+      }
     }
+    return elves.max()
+  }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+  fun part2(input: List<String>): Int {
+    val elves = mutableListOf<Int>()
+    var totalCalories = 0
+    for(food in input) {
+      if (food != "") {
+        totalCalories += food.toInt()
+      } else {
+        elves.add(totalCalories)
+        totalCalories = 0
+      }
     }
+    elves.add(totalCalories)
+    totalCalories = 0
+    println(elves)
+    for(i in 1..3) {
+      totalCalories += elves.max()
+      elves.remove(elves.max())
+    }
+    return totalCalories
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("Day01_test")
+  println(part1(testInput))
+  println(part2(testInput))
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  val input = readInput("Day01")
+  output(part1(input))
+  output(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,3 +1,5 @@
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
@@ -5,7 +7,24 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(name: String) : List<String> {
+    val file = File("src", "$name.txt")
+    if(!file.exists()) {
+        file.writeText("")
+    }
+    return file.readLines()
+}
+
+fun output(o: Any) {
+    println(o)
+
+    Toolkit.getDefaultToolkit()
+        .getSystemClipboard()
+        .setContents(
+            StringSelection(o.toString()),
+            null
+        )
+}
 
 /**
  * Converts string to md5 hash.
